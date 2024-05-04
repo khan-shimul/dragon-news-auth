01927a19be17c959a3040303b14942bd99f178a4
khan-shimul <khanshimul196@gmail.com>
2024-05-05 00:23:15 +0600

    desiged category

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("/categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
     <div>
-      <h2 className="text-2xl">Left Side Nav</h2>
+      <div className="space-y-6">
+        <h2 className="text-2xl font-bold">All Categories</h2>
+        {categories.map((category) => (
+          <NavLink
+            key={category.id}
+            to={`/category/${category.id}`}
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "bg-red-400"
+                : "block font-medium ml-4"
+            }
+          >
+            {category.name}
+          </NavLink>
+        ))}
+      </div>
     </div>
   );
 };
